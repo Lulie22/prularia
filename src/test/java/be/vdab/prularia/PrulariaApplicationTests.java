b801215bfac6f03aaab78e631f78a79bc5341d85
Lulie <lulie08@gmail.com>
2022-10-14 20:01:33 +0200

    initial setup of PrulariaApplication

---
+++ src/test/java/be/vdab/prularia/PrulariaApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.prularia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrulariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
