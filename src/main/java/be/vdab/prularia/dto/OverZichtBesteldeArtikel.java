b2267c6d521585a37310191e7ed0cb3087e3c8a9
SvenBouwen <sven.bouwen@vdabcampus.be>
2022-10-18 10:05:00 +0200

    aanpassing OverzichtBesteldArtikel (zelfde benamingen als in tabel)

--- src/main/java/be/vdab/prularia/dto/OverZichtBesteldeArtikel.java
+++
@@ -1,4 +0,0 @@
-package be.vdab.prularia.dto;
-
-public record OverZichtBesteldeArtikel(String artikelNaam, int rijNummer, int rekNummer, int aantal, boolean aangevinkt ) {
-}
---
+++ src/main/java/be/vdab/prularia/dto/OverzichtBesteldArtikel.java
@@ -0,0 +1,4 @@
+package be.vdab.prularia.dto;
+
+public record OverzichtBesteldArtikel(long artikelId, String naam, char rij, int rek, int aantal, boolean aangevinkt ) {
+}
