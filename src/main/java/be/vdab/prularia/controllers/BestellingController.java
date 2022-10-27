bf58057f457a227ca558f2bf9dc94685e79fe5c8
Lulie <lulie08@gmail.com>
2022-10-27 12:52:48 +0200

    controller implmentatie voor afwerkingbestellingen

--- src/main/java/be/vdab/prularia/controllers/BestellingController.java
+++ src/main/java/be/vdab/prularia/controllers/BestellingController.java
@@ -1,30 +1,34 @@
 package be.vdab.prularia.controllers;
 
-import be.vdab.prularia.exceptions.GeenVolgendeBestellingException;
-import be.vdab.prularia.exceptions.OnvoldoendeArtikelInHetMagazijnException;
+import be.vdab.prularia.exceptions.*;
+import be.vdab.prularia.services.BestellijnService;
 import be.vdab.prularia.services.BestellingService;
 import be.vdab.prularia.sessions.MagazijnierSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("bestelling")
 public class BestellingController {
     private final BestellingService bestellingService;
     private final MagazijnierSession magazijnierSession;
+    private final BestellijnService bestellijnService;
 
-    public BestellingController(BestellingService bestellingService, MagazijnierSession magazijnierSession) {
+    public BestellingController(BestellingService bestellingService, MagazijnierSession magazijnierSession, BestellijnService bestellijnService) {
         this.bestellingService = bestellingService;
         this.magazijnierSession = magazijnierSession;
+        this.bestellijnService = bestellijnService;
     }
 
     // voor een nieuwe bestelling op te vragen
     @GetMapping("nieuw")
     public ModelAndView vindVolgendeBestelling() {
         // Check of er een volgende bestelling is adhv bestelId
-            // indien er geen is, vraag en check de nieuwe bestelling
+        // indien er geen is, vraag en check de nieuwe bestelling
         if (magazijnierSession.getLijstVanBesteldeArtikels() == null) {
             try {
                 bestellingService.vindVolgendeBestelling();
@@ -45,4 +49,33 @@ public class BestellingController {
                 .addObject("lijstVanBesteldeArtikels", magazijnierSession.getLijstVanBesteldeArtikels());
     }
 
+    @PostMapping("afgewerktebestelling")
+
+    public ModelAndView afgewerktebestelling(RedirectAttributes redirect) {
+       // check();
+
+        try {
+            var uitgaandeLeveringsId = bestellingService.afgewerkteBestelling();
+            redirect.addAttribute("uitgaandeLeveringsId", uitgaandeLeveringsId);
+            return new ModelAndView("redirect:/");
+        } catch (OnvoldoendeArtikelInHetMagazijnException ex) {
+            redirect.addAttribute("onvoldoendeArtikelenInMagazijn", true);
+            redirect.addAttribute("onvoldoendeArtikelenBestelId", ex.getBestelId());
+            redirect.addAttribute("onvoldoendeArtikelenBestellijnId", ex.getBestellijnId());
+            redirect.addAttribute("onvoldoendeArtikelenMagazijnPlaatsId", ex.getMagazijnPlaatsId());
+            redirect.addAttribute("onvoldoendeArtikelenArtikelId", ex.getArtikelId());
+            return new ModelAndView("redirect:/");
+        } catch (OnvoldoendeArtikelVoorraadException ex) {
+            //
+            redirect.addAttribute("onvoldoendeArtikelVoorraad", true);
+            redirect.addAttribute("onvoldoendeArtikelenBestelId", ex.getBestelId());
+            redirect.addAttribute("onvoldoendeArtikelenBestellijnId", ex.getBestellijnId());
+            redirect.addAttribute("onvoldoendeArtikelenArtikelId", ex.getArtikelId());
+            return new ModelAndView("redirect:/");
+        } catch (MagazijnPlaatNietGevondenException ex) {
+            redirect.addAttribute("magazijnPlaatsnietGevonden", true);
+            return new ModelAndView("redirect:/");
+        }
+
+    }
 }
